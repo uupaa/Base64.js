3724f522e63629fc3bb1b4f6b5cf099cc241369d
uupaa <uupaa.js@gmail.com>
2014-03-18 22:54:06 +0900

    update settings

--- lint/plato/report.js
+++ lint/plato/report.js
@@ -1,1 +1,63 @@
-__report = {"summary":{"total":{"sloc":229,"maintainability":61.63911840509302},"average":{"sloc":229,"maintainability":"61.64"}},"reports":[{"info":{"file":"lib/Base64.js","fileShort":"lib/Base64.js","fileSafe":"lib_Base64_js","link":"files/lib_Base64_js/index.html"},"jshint":{"messages":3},"complexity":{"aggregate":{"line":4,"complexity":{"sloc":{"physical":229,"logical":105},"cyclomatic":18,"halstead":{"operators":{"distinct":32,"total":310,"identifiers":["__stripped__"]},"operands":{"distinct":107,"total":391,"identifiers":["__stripped__"]},"length":701,"vocabulary":139,"difficulty":58.467289719626166,"volume":4990.377691979179,"effort":291773.858327306,"bugs":1.6634592306597262,"time":16209.658795961444},"params":14}},"module":"lib/Base64.js","maintainability":61.63911840509302}}]}
+__report = {
+  "reports": [
+    {
+      "info": {
+        "file": "lib/Base64.js",
+        "fileShort": "lib/Base64.js",
+        "fileSafe": "lib_Base64_js",
+        "link": "files/lib_Base64_js/index.html"
+      },
+      "jshint": {
+        "messages": 3
+      },
+      "complexity": {
+        "aggregate": {
+          "line": 4,
+          "complexity": {
+            "sloc": {
+              "physical": 229,
+              "logical": 105
+            },
+            "cyclomatic": 18,
+            "halstead": {
+              "operators": {
+                "distinct": 32,
+                "total": 310,
+                "identifiers": [
+                  "__stripped__"
+                ]
+              },
+              "operands": {
+                "distinct": 107,
+                "total": 391,
+                "identifiers": [
+                  "__stripped__"
+                ]
+              },
+              "length": 701,
+              "vocabulary": 139,
+              "difficulty": 58.467289719626166,
+              "volume": 4990.377691979179,
+              "effort": 291773.858327306,
+              "bugs": 1.6634592306597262,
+              "time": 16209.658795961444
+            },
+            "params": 14
+          }
+        },
+        "module": "lib/Base64.js",
+        "maintainability": 61.63911840509302
+      }
+    }
+  ],
+  "summary": {
+    "total": {
+      "sloc": 229,
+      "maintainability": 61.63911840509302
+    },
+    "average": {
+      "sloc": 229,
+      "maintainability": "61.64"
+    }
+  }
+}
