87b2f2d27eff9ad2a0a49812a989b049fdf0f65e
uupaa <uupaa.js@gmail.com>
2014-04-06 16:10:03 +0900

    follow WebModule spec

--- test/index.node.js
+++ test/index.node.js
@@ -3,6 +3,6 @@ require("uupaa.task.js");
 require("uupaa.test.js");
 require("uupaa.bytearray.js");
 require("../lib/Base64.js");
-require("../lib/Base64.min.js");
+require("../release/Base64.min.js");
 require("./test.js");
 
