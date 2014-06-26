52dc26853561959ce50558a9f35bc2a09f0e7985
uupaa <uupaa.js@gmail.com>
2014-06-27 03:14:13 +0900

    remove Array methods

--- lint/plato/report.js
+++ lint/plato/report.js
@@ -8,56 +8,56 @@ __report = {
         "link": "files/lib_Base64_js/index.html"
       },
       "jshint": {
-        "messages": 1
+        "messages": 0
       },
       "complexity": {
         "aggregate": {
           "line": 1,
           "complexity": {
             "sloc": {
-              "physical": 296,
-              "logical": 148
+              "physical": 185,
+              "logical": 96
             },
-            "cyclomatic": 29,
+            "cyclomatic": 20,
             "halstead": {
               "operators": {
-                "distinct": 35,
-                "total": 447,
+                "distinct": 28,
+                "total": 275,
                 "identifiers": [
                   "__stripped__"
                 ]
               },
               "operands": {
-                "distinct": 216,
-                "total": 739,
+                "distinct": 185,
+                "total": 535,
                 "identifiers": [
                   "__stripped__"
                 ]
               },
-              "length": 1186,
-              "vocabulary": 251,
-              "difficulty": 59.87268518518518,
-              "volume": 9454.250654985615,
-              "effort": 566051.3731277846,
-              "bugs": 3.151416884995205,
-              "time": 31447.29850709914
+              "length": 810,
+              "vocabulary": 213,
+              "difficulty": 40.48648648648649,
+              "volume": 6265.114792382929,
+              "effort": 253652.4853780981,
+              "bugs": 2.0883715974609767,
+              "time": 14091.804743227673
             },
-            "params": 19
+            "params": 15
           }
         },
         "module": "lib/Base64.js",
-        "maintainability": 60.532449770988244
+        "maintainability": 62.83196244931614
       }
     }
   ],
   "summary": {
     "total": {
-      "sloc": 296,
-      "maintainability": 60.532449770988244
+      "sloc": 185,
+      "maintainability": 62.83196244931614
     },
     "average": {
-      "sloc": 296,
-      "maintainability": "60.53"
+      "sloc": 185,
+      "maintainability": "62.83"
     }
   }
 }
--- test/test.js
+++ test/test.js
@@ -11,27 +11,26 @@ return new Test("Base64", {
         testBase64,
         testBase64EncodeAndDecode,
         testBase64atobAndbtoa,
-        testURLSafe64,
         testRandom,
     ]).run().clone();
 
-function testBase64(next) {
+function testBase64(test, pass, miss) {
 
     var source = "1234567890ABCDEFGHIJKLMN";
     var base64 = Base64(source); // "MTIzNDU2Nzg5MEFCQ0RFRkdISUpLTE1O"
     var revert = Base64.atob(base64);
 
     if (source === revert) {
-        next && next.pass();
+        test.done(pass());
     } else {
-        next && next.miss();
+        test.done(miss());
     }
 }
 
-function testBase64EncodeAndDecode(next) {
+function testBase64EncodeAndDecode(test, pass, miss) {
 
     function _test(source) {
-        var base64 = Base64.encode( DataType["Array"].fromString(source) );
+        var base64 = Base64.encode( DataType["Uint8Array"].fromString(source) );
         var revert = DataType["Array"].toString( Base64.decode(base64) );
 
         return source === revert;
@@ -42,153 +41,47 @@ function testBase64EncodeAndDecode(next) {
     if (_test(source) &&
         _test(source + source) &&
         _test((source + source).slice(2, 20))) {
-        next && next.pass();
+        test.done(pass());
     } else {
-        next && next.miss();
+        test.done(miss());
     }
 }
 
-function testBase64atobAndbtoa(next) {
+function testBase64atobAndbtoa(test, pass, miss) {
 
     var source = "1234567890ABCDEFGHIJKLMN";
     var base64 = Base64.btoa(source); // "MTIzNDU2Nzg5MEFCQ0RFRkdISUpLTE1O"
     var revert = Base64.atob(base64);
 
     if (source === revert) {
-        next && next.pass();
+        test.done(pass());
     } else {
-        next && next.miss();
+        test.done(miss());
     }
 }
 
-function testURLSafe64(next) {
-
-    var source = "0=~|"; // -> URLSafe64("0=~|") -> "MD1-fA"
-                         // ->    Base64("0=~|") -> "MD1+fA=="
-    var urlsafe64 = Base64.toURLSafe64( Base64.encode( DataType["Array"].fromString(source) ) );
-    var revert = DataType["Array"].toString( Base64.decode( Base64.fromURLSafe64( urlsafe64 ) ) );
-
-    if (source !== revert || /[\+\/\=]/.test(urlsafe64)) {
-        next && next.miss();
-    } else {
-        next && next.pass();
-    }
-}
-
-function testRandom(next) {
-
-    function _test(source) {
-        var base64 = Base64.encode( source );
-        var revert = Base64.decode( base64 );
-
-        return JSON.stringify(source) === JSON.stringify(revert);
-    }
-
-    function _testEncodeArray(source) {
-        var base64 = Base64.encodeArray( source );
-        var revert = Base64.decodeArray( base64 );
-
-        return JSON.stringify(Array.prototype.slice.call(source)) ===
-               JSON.stringify(Array.prototype.slice.call(revert));
-    }
+function testRandom(test, pass, miss) {
 
     function _testEncodeTypedArray(source) {
         var typedSource = new Uint8Array(source);
-        var typedBase64 = Base64.encodeTypedArray( typedSource );
-        var typedRevert = Base64.decodeTypedArray( typedBase64 );
+        var typedBase64 = Base64.encode( typedSource );
+        var typedRevert = Base64.decode( typedBase64 );
 
         return JSON.stringify(Array.prototype.slice.call(typedSource)) ===
                JSON.stringify(Array.prototype.slice.call(typedRevert));
     }
 
-    function _testMatch(source) {
-        var typedSource      = new Uint8Array(source);
-
-        var base64Array      = Base64.encodeArray( source );
-        var base64TypedArray = Base64.encodeTypedArray( typedSource );
-
-
-        var revertArray      = Base64.decodeArray( base64Array );
-        var revertTypedArray = Base64.decodeTypedArray( base64TypedArray );
-
-        var fail  = 0x00;
-
-        if ( JSON.stringify(Array.prototype.slice.call(base64Array)) !==
-             JSON.stringify(Array.prototype.slice.call(base64TypedArray)) ) {
-            fail |= 0x01;
-        }
-        if ( JSON.stringify(Array.prototype.slice.call(revertArray)) !==
-             JSON.stringify(Array.prototype.slice.call(revertTypedArray)) ) {
-            fail |= 0x02;
-        }
-        if ( base64Array.length !== base64TypedArray.length ) {
-            fail |= 0x04;
-        }
-        if ( revertArray.length !== revertTypedArray.length ) {
-            fail |= 0x08;
-        }
-
-        if (fail) {
-            if (fail & 0x01) { console.log("encode fail"); }
-            if (fail & 0x02) { console.log("decode fail"); }
-            if (fail & 0x04) { console.log("encode length missmatch"); }
-            if (fail & 0x08) { console.log("decode length missmatch"); }
-
-            if (fail & 0x05) { // encode fail
-                var i = 0, iz = Math.max(base64Array.length, base64TypedArray.length);
-
-                for (; i < iz; ++i) {
-                    if (base64Array[i] !== base64TypedArray[i]) {
-                        debugger;
-                        console.log("encode fail. index = " + i);
-                        break;
-                    }
-                }
-            }
-            if (fail & 0x0a) { // decode fail
-                var i = 0, iz = Math.max(revertArray.length, revertTypedArray.length);
-
-                for (; i < iz; ++i) {
-                    if (revertArray[i] !== revertTypedArray[i]) {
-                        debugger;
-                        console.log("decode fail. index = " + i);
-                        break;
-                    }
-                }
-            }
-            return false;
-        }
-
-        return fail ? false : true;
-    }
-
     function _random(times) {
+        var decimal = true;
 
         for (var i = 0, iz = times; i < iz; ++i) {
-            var jz = (random.value() & 0xff) + 10;
+            var jz = (random.value(decimal) & 0xff) + 10;
             var ary = [];
 
             for (var j = 0; j < jz; ++j) {
-                ary.push( random.value() & 0xff );
-            }
-            if ( !_test(ary) ) {
-                debugger;
-                _test(ary);
-                return false;
-            }
-            if ( !_testEncodeArray(ary) ) {
-                debugger;
-                _testEncodeArray(ary);
-                return false;
+                ary.push( random.value(decimal) & 0xff );
             }
             if ( !_testEncodeTypedArray(ary) ) {
-                debugger;
-                _testEncodeTypedArray(ary);
-                return false;
-            }
-            if ( !_testMatch(ary) ) {
-                debugger;
-                _testMatch(ary);
                 return false;
             }
         }
@@ -199,9 +92,9 @@ function testRandom(next) {
     var times = 1000;
 
     if (_random(times)) {
-        next && next.pass();
+        test.done(pass());
     } else {
-        next && next.miss();
+        test.done(miss());
     }
 }
 
